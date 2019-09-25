2ddb1b80692c61216064e896352bcbefa8b50be2
Rider-Maru <hiroto.rider@outlook.jp>
2019-09-25 12:49:21 +0900

    Update henshin.js

--- henshin.js
+++ henshin.js
@@ -1,4 +1,4 @@
-var ClickNum = 1;
+var ClickNum = 0;
 var SE_authorize = document.getElementById("Sound_Zero-One:2");
 var SE_standby = document.getElementById("Sound_Zero-One:standby");
 
@@ -10,9 +10,16 @@ var onStandBy = false;
 // 効果音を鳴らす（★今回のメインはこれ★）
 // ========================================
 function ring() {
-    document.getElementById("Sound_Zero-One:" + ClickNum).play();
     if (onStandBy) SEstandbyStop();
-    if (ClickNum == 3) ClickNum = 0;
+    if (ClickNum == 4) ClickNum = 1;
+    if (ClickNum == 0) {
+        for (var i = 1; i < 4; i++) {
+            document.getElementById("Sound_Zero-One:" + i).play();
+        }
+    }
+    else {
+        document.getElementById("Sound_Zero-One:" + ClickNum).play();
+    }
     ClickNum++;
 }
 SE_authorize.addEventListener("ended", function () {
