723f646541f4c02ffb82cb5c4a38427acf03717c
Rider-Maru <hiroto.rider@outlook.jp>
2019-09-27 02:40:25 +0900

    カメラを外側にした

--- henshin.js
+++ henshin.js
@@ -9,9 +9,9 @@ var onAuthorize = false;
 var video = document.getElementById("video");
 //取得するメディア情報を指定
 var medias = { audio: false, video: {} };
-medias.video.facingMode = { exact: "user" };
-//medias.video.facingMode = { exact: "environment" };
-document.getElementById("str").textContent = "user";
+//medias.video.facingMode = { exact: "user" };
+medias.video.facingMode = { exact: "environment" };
+document.getElementById("str").textContent = "environment";
 
 //getUserMediaを用いて、webカメラの映像を取得
 navigator.mediaDevices.getUserMedia(medias).then(
