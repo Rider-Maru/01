c9832aac3e0ce26201befaa96b7c3d171f8ffc59
Rider-Maru <hiroto.rider@outlook.jp>
2019-09-27 09:10:15 +0900

    Revert "元に戻した"
    This reverts commit 804dca5e32abf93a4dcc828457e97e777c0ece80.

--- henshin.js
+++ henshin.js
@@ -98,23 +98,26 @@ function ring() {
     else {
         isPushKey = true;
         AutorizeNum = 2;
-        document.getElementById("Sound_Zero-One:" + ClickNum).currentTime = 0;
-        document.getElementById("Sound_Zero-One:" + ClickNum).play();
+        //document.getElementById("Sound_Zero-One:" + ClickNum).currentTime = 0;
+        //document.getElementById("Sound_Zero-One:" + ClickNum).play();
+        SE_authorize.currentTime=0;
+        SE_authorize.play();
     }
     //ClickNum++;
 }
 
 function ringByCamera() {
     if (AutorizeNum < 4 && (AutorizeNum != 3 || isProgrisable)) {
-        if (onStandBy) SEstandbyStop();
-        document.getElementById("Sound_Zero-One:" + AutorizeNum).currentTime = 0;
-        document.getElementById("Sound_Zero-One:" + AutorizeNum).play();
+        //if (onStandBy) SEstandbyStop();
+        //document.getElementById("Sound_Zero-One:" + AutorizeNum).currentTime = 0;
+        //document.getElementById("Sound_Zero-One:" + AutorizeNum).play();
+        SE_authorize.currentTime = 1;
 
-        isProgrisable = false;
+        //isProgrisable = false;
         setTimeout(function () {
             isProgrisable = true;
-        }, 3000)
-        AutorizeNum++;
+        }, 0)
+        //AutorizeNum++;
     }
     
 }
