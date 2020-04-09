957c825b3b848c0fc87ca0c99f0b768827a33c12
Rider-Maru <hiroto.rider@outlook.jp>
2020-04-10 01:43:53 +0900

    Update henshin.js

--- henshin.js
+++ henshin.js
@@ -142,7 +142,9 @@ function ringByCamera(callNum) {
 
                 isAuthorizable = false;
                 setTimeout(function () {
+                    Console("changeAutorizeable");
                     if (onRingingStandbyLetRise || AutorizeNum == 3) isAuthorizable = true;
+                    console(isAuthorizable);
                 }, 1500)
             }
             else playSECallFunction(mySwiper.realIndex);
@@ -170,6 +172,7 @@ function SEstandbyStop() {
     //SE_progrise.pause();
     //SE_progrise.currentTime = 0;
     onStandBy = false;
+    onStandByMetal = false;
     
     stopSE();
     stopStandbySE();
