9092e8658e2090bef9548399029171965bea88aa
Rider-Maru <hiroto.rider@outlook.jp>
2020-05-18 23:13:17 +0900

    Update henshin.js

--- henshin.js
+++ henshin.js
@@ -138,6 +138,7 @@ function ringByCamera(callNum) {
             isRing = true;
             releaseCamera = false;
             playSEfinishRise(mySwiper.realIndex);
+            playSEfinishRise(mySwiper.realIndex);
         }
      }
     else if (callNum == 2) {
